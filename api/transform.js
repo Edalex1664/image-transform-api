18ae5a7f870ff4c0ddfbd3aa149cfee7ec59294f
Edalex1664 <alexisbonnetsalkind@gmail.com>
2025-07-21 21:49:22 +0200

    Update transform.js

--- api/transform.js
+++ api/transform.js
@@ -13,7 +13,7 @@ module.exports = async function handler(req, res) {
     const response = await fetch("https://api.replicate.com/v1/predictions", {
       method: "POST",
       headers: {
-        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+        Authorization: `Token ${process.env.REPLICATE_API_TOKENtu}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
